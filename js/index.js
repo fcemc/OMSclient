e358ed215567db2fd53e6bd5f654cc86a1b1122d
Jerry Simmons GISP <jerrysimmons@fourcty.org>
2016-04-26 14:02:56 -0400

    Added files via upload

--- js/index.js
+++ js/index.js
@@ -25,6 +25,8 @@ var app = {
 };
 
 function onDeviceReady() {
+    document.addEventListener("resume", onResume, false);
+
     document.addEventListener("backbutton", function (e) {
         if ($("#home").length > 0) {
             // call this to get a new token each time. don't call it to reuse existing token.
@@ -35,7 +37,8 @@ function onDeviceReady() {
         else {
             navigator.app.backHistory();
         }
-    }, false);
+    }, false);   
+
 
     try {
         pushNotification = window.plugins.pushNotification;
@@ -53,88 +56,92 @@ function onDeviceReady() {
     }
 }
 
-// handle APNS notifications for iOS
-function onNotificationAPN(e) {
-    if (e.alert) {
-        // showing an alert also requires the org.apache.cordova.dialogs plugin
-        navigator.notification.alert(e.alert);
+function onResume() {
+    window.location.reload();
+}
+
+    // handle APNS notifications for iOS
+    function onNotificationAPN(e) {
+        if (e.alert) {
+            // showing an alert also requires the org.apache.cordova.dialogs plugin
+            navigator.notification.alert(e.alert);
+        }
+
+        if (e.sound) {
+            // playing a sound also requires the org.apache.cordova.media plugin
+            var snd = new Media(e.sound);
+            snd.play();
+        }
+
+        if (e.badge) {
+            pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+        }
     }
 
-    if (e.sound) {
-        // playing a sound also requires the org.apache.cordova.media plugin
-        var snd = new Media(e.sound);
-        snd.play();
+    function tokenHandler(result) {
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.    
+        localStorage.setItem("fcemcOMS_MEM_clientType", "iOS");
+        localStorage.setItem("fcemcOMS_MEM_did", result);
+        localStorage.setItem("fcemcOMS_MEM_uuid", device.uuid);
+
     }
 
-    if (e.badge) {
-        pushNotification.setApplicationIconBadgeNumber(successHandler, e.badge);
+
+    // handle GCM notifications for Android
+    function onNotification(e) {
+        switch (e.event) {
+            case 'registered':
+                if (e.regid.length > 0) {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    localStorage.setItem("fcemcOMS_MEM_clientType", "Android");
+                    localStorage.setItem("fcemcOMS_MEM_did", e.regid);
+                    localStorage.setItem("fcemcOMS_MEM_uuid", device.uuid);
+                }
+                break;
+
+            case 'message':
+                // if this flag is set, this notification happened while we were in the foreground.
+                // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+                if (e.foreground) {
+                    // on Android soundname is outside the payload. 
+                    // On Amazon FireOS all custom attributes are contained within payload
+                    var soundfile = e.soundname || e.payload.sound;
+                    // if the notification contains a soundname, play it.
+                    // playing a sound also requires the org.apache.cordova.media plugin
+                    var my_media = new Media("/android_asset/www/" + soundfile);
+                    my_media.play();
+                }
+                else {	// otherwise we were launched because the user touched a notification in the notification tray.
+                    if (e.coldstart)
+                        var e = e.coldstart;
+                    else
+                        //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');        
+                        var e = e.coldstart;
+                }
+
+                //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+                //android only
+                //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+                //amazon-fireos only
+                //$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
+                break;
+
+            case 'error':
+                var e = e.msg;
+                break;
+
+            default:
+                $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+                break;
+        }
     }
-}
 
-function tokenHandler(result) {
-    // Your iOS push server needs to know the token before it can push to this device
-    // here is where you might want to send it the token for later use.    
-    localStorage.setItem("fcemcOMS_MEM_clientType", "iOS");
-    localStorage.setItem("fcemcOMS_MEM_did", result);
-    localStorage.setItem("fcemcOMS_MEM_uuid", device.uuid);
-
-}
-
-
-// handle GCM notifications for Android
-function onNotification(e) {
-    switch (e.event) {
-        case 'registered':
-            if (e.regid.length > 0) {
-                // Your GCM push server needs to know the regID before it can push to this device
-                // here is where you might want to send it the regID for later use.
-                localStorage.setItem("fcemcOMS_MEM_clientType", "Android");
-                localStorage.setItem("fcemcOMS_MEM_did", e.regid);
-                localStorage.setItem("fcemcOMS_MEM_uuid", device.uuid);
-            }
-            break;
-
-        case 'message':
-            // if this flag is set, this notification happened while we were in the foreground.
-            // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-            if (e.foreground) {
-                // on Android soundname is outside the payload. 
-                // On Amazon FireOS all custom attributes are contained within payload
-                var soundfile = e.soundname || e.payload.sound;
-                // if the notification contains a soundname, play it.
-                // playing a sound also requires the org.apache.cordova.media plugin
-                var my_media = new Media("/android_asset/www/" + soundfile);
-                my_media.play();
-            }
-            else {	// otherwise we were launched because the user touched a notification in the notification tray.
-                if (e.coldstart)
-                    var e = e.coldstart;
-                else
-                    //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');        
-                    var e = e.coldstart;
-            }
-
-            //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-            //android only
-            //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-            //amazon-fireos only
-            //$("#app-status-ul").append('<li>MESSAGE -> TIMESTAMP: ' + e.payload.timeStamp + '</li>');
-            break;
-
-        case 'error':
-            var e = e.msg;
-            break;
-
-        default:
-            $("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
-            break;
+    function successHandler(result) {
+        var r = result;
     }
-}
 
-function successHandler(result) {
-    var r = result;
-}
-
-function errorHandler(error) {
-    var r = result;
-}
+    function errorHandler(error) {
+        var r = result;
+    }
